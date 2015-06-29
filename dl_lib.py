4d8ecd54667556c96b87e83dfbd2fb79c6827ddc
Takeshi <a86487817@gmail.com>
2015-06-30 01:57:55 +0800

    add list, install function

---
+++ dl_lib.py
@@ -0,0 +1,68 @@
+import contextlib
+import pdb
+from ryu.lib.hub import StreamServer
+from ryu.lib import hub
+import json
+from ryu.base import app_manager
+from ryu.controller import event
+
+class EventAppListRequst(event.EventRequestBase):
+
+    def __init__(self):
+        super(EventAppListRequst, self).__init__()
+        self.dst = 'DynamicLoader'
+
+class EventAppListReply(event.EventReplyBase):
+    '''
+    reply application application list
+    ex:
+    [
+        {app name(string): (app class, installed(boolean))},
+        ...
+    ]
+    '''
+    def __init__(self, dst, app_list):
+        super(EventAppListReply, self).__init__(dst)
+        self.app_list = app_list
+
+class EventAppInstall(event.EventBase):
+
+    def __init__(self, app_id):
+        super(EventAppInstall, self).__init__()
+        self.app_id = app_id
+
+class DynamicLoaderLib(app_manager.RyuApp):
+
+    def __init__(self, *args, **kwargs):
+        super(DynamicLoaderLib, self).__init__(*args, **kwargs)
+        self.name = 'dl_lib'
+        self.server = StreamServer(('0.0.0.0', 10807), self._connection_factory)
+
+    def start(self):
+        hub.spawn(self.server.serve_forever)
+
+    def _connection_factory(self, socket, address):
+
+
+        try:
+            while True:
+                buf = socket.recv(128)
+                try:
+                    msg = json.loads(buf.decode('ascii'))
+
+                    if msg['cmd'] == 'list':
+                        req = EventAppListRequst()
+                        rep = self.send_request(req)
+                        reply_msg = json.dumps(rep.app_list)
+                        socket.sendall(reply_msg)
+
+                    if msg['cmd'] == 'install':
+                        ev = EventAppInstall(msg['app_id'])
+                        self.send_event_to_observers(ev)
+
+
+                except Exception:
+                    pass
+                hub.sleep(0.1)
+        finally:
+            socket.close()
---
+++ dl_plugin.py
@@ -0,0 +1,112 @@
+import logging
+import pkgutil
+import inspect
+import dl_lib
+from ryu.base import app_manager
+from ryu.controller.handler import MAIN_DISPATCHER
+from ryu.controller.handler import set_ev_cls
+from ryu import app as ryu_app
+from ryu.base.app_manager import RyuApp, AppManager
+
+
+LOG = logging.getLogger('DynamicLoader')
+
+def deep_import(mod_name):
+    mod = __import__(mod_name)
+    components = mod_name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+class DynamicLoader(app_manager.RyuApp):
+    _CONTEXTS = {
+        'dl_lib': dl_lib.DynamicLoaderLib
+    }
+    def __init__(self, *args, **kwargs):
+        super(DynamicLoader, self).__init__(*args, **kwargs)
+        self.ryu_mgr = AppManager.get_instance()
+        self.available_app = []
+        for _, name, is_pkg in pkgutil.walk_packages(ryu_app.__path__):
+            LOG.debug(
+                'Find %s : %s',
+                'package' if is_pkg else 'module',
+                name)
+
+            if is_pkg:
+                continue
+
+            try:
+                _app_module = deep_import('ryu.app.' + name)
+
+                for _attr_name in dir(_app_module):
+                    _attr = getattr(_app_module, _attr_name)
+
+                    if inspect.isclass(_attr) and _attr.__bases__[0] == RyuApp:
+                        LOG.debug('\tFind ryu app : %s.%s', _attr.__module__, _attr.__name__)
+                        _full_name = '%s.%s' % (_attr.__module__, _attr.__name__)
+                        self.available_app.append((_full_name, _attr))
+
+                
+
+
+            except ImportError:
+                LOG.debug('Import Error')
+
+
+    @set_ev_cls(dl_lib.EventAppListRequst, MAIN_DISPATCHER)
+    def list_handler(self, req):
+        res = []
+        _installed_apps = self.ryu_mgr.applications
+
+        for app_info in self.available_app:
+            _cls = app_info[1]
+
+            if _cls in \
+                [obj.__class__ for obj in _installed_apps.values()]:
+                res.append({'name': app_info[0], 'installed': True})
+
+            else:
+                res.append({'name': app_info[0], 'installed': False})
+
+
+        rep = dl_lib.EventAppListReply(req.src, res)
+        self.reply_to_request(req, rep)
+
+
+    @set_ev_cls(dl_lib.EventAppInstall, MAIN_DISPATCHER)
+    def install_handler(self, ev):
+        '''Install ryu application'''
+      
+
+        try:
+            app_id = ev.app_id
+            print('Installing app-id %d' % (app_id, ))
+            app_cls = self.available_app[app_id][1]
+            _installed_apps = self.ryu_mgr.applications
+
+            if app_cls in \
+                [obj.__class__ for obj in _installed_apps.values()]:
+                # app was installed
+                print('Application already installed')
+
+            app = self.ryu_mgr.instantiate(app_cls)
+            app.start()
+
+            print('install finished.')
+
+        except IndexError:
+            print('Can\'t find application with id %d' % app_id)
+
+        except ValueError:
+            print('ryu-app-id must be number')
+
+        except Exception, ex:
+            print 'Import error'
+            raise ex
+
+
+
+
+
+
+
