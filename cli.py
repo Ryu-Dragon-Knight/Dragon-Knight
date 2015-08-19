440ba0d4efa41a6972e9027954b2d739ca87330d
Takeshi <a86487817@gmail.com>
2015-08-19 22:17:54 +0800

    new version of command line interface

--- cli.py
+++ cli.py
@@ -1,62 +1,120 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 # -*- codeing: utf-8 -*-
-import socket
-import logging
+from __future__ import print_function
+
+import cmd
+import six
 import json
 
-LOG = logging.getLogger('DynamicLoadCmd')
+if six.PY2:
+    import urllib2 as urllib
+
+else:
+    import urllib3 as urllib
 
 
-def main():
-    
-    sc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sc.connect(('127.0.0.1', 10807))
+def http_get(url):
+    '''
+    do http GET method
+    return python dictionary data
 
-    while True:
-        line = raw_input('(ryu) ')
+    param:
+        url: url for http GET, string type
+    '''
+    result = None
 
-        if line == 'exit':
-            break
+    if six.PY2:
+        response = urllib.urlopen(url)
+        result = json.load(response)
 
-        elif line == 'list':
-            msg = json.dumps({'cmd': 'list'})
-            sc.sendall(msg)
-            buf = sc.recv(2048)
-            app_list = json.loads(buf)
-            app_id = 0
+    else:
+        http = urllib.PoolManager()
+        response = http.request('GET', url)
+        result = json.loads(response.data.decode('utf8'))
 
-            for app_info in app_list:
-                print '[%02d]%s' % (app_id, app_info['name']),
+    return result
 
-                if app_info['installed']:
-                    print '[\033[92minstalled\033[0m]'
-                else:
-                    print ''
+def http_post(url, req_body):
+    '''
+    do http POST method
+    return python dictionary data
 
-                app_id += 1
+    param:
+        url: url for http GET, string type
+        req_body: data to send, string type
+    '''
+    result = None
 
-        elif 'uninstall' in line:
-            argv = line.split(' ')
+    if six.PY2:
+        response = urllib.urlopen(url, data=req_body)
+        result = json.load(response)
 
-            if len(argv) < 2:
-                print 'install [app_id]'
-                continue
+    else:
+        http = urllib.PoolManager()
+        response = http.urlopen('POST', url, body=req_body)
+        result = json.load(response)
 
-            app_id = int(argv[1])
-            msg = json.dumps({'cmd':'uninstall', 'app_id': app_id})
-            sc.sendall(msg)
+    return result
 
-        elif 'install' in line:
-            argv = line.split(' ')
+class DlCli(cmd.Cmd):
+    """
+    Ryu dynamic loader command line
+    """
 
-            if len(argv) < 2:
-                print 'install [app_id]'
-                continue
+    def do_list(self, line):
+        '''
+        List all available applications.
+        '''
 
-            app_id = int(argv[1])
-            msg = json.dumps({'cmd':'install', 'app_id': app_id})
-            sc.sendall(msg)
+        app_list = http_get('http://127.0.0.1:8080/list')
+        app_id = 0
 
+        for app_info in app_list:
+            print('[%02d]%s' % (app_id, app_info['name']), end='')
+
+            if app_info['installed']:
+                print('[\033[92minstalled\033[0m]')
+            else:
+                print('')
+
+            app_id += 1
+
+
+    def do_install(self, line):
+        '''
+        Install ryu application
+        Usage:
+            install [app_id]
+        '''
+        try:
+            app_id = int(line)
+            req_body = json.dumps({'app_id':app_id})
+            result = http_post('http://127.0.0.1:8080/install', req_body)
+            print(result)
+
+        except ValueError:
+            print('Application id must be integer')
+
+    def do_uninstall(self, line):
+        '''
+        Uninstall ryu application
+        Usage:
+            uninstall [app_id]
+        '''
+        try:
+            app_id = int(line)
+            req_body = json.dumps({'app_id':app_id})
+            result = http_post('http://127.0.0.1:8080/uninstall', req_body)
+            print(result)
+
+        except ValueError:
+            print('Application id must be integer')
+
+    def do_exit(self, line):
+        '''
+        Exit from command line
+        '''
+        return True
 
 if __name__ == '__main__':
-    main()
+    DlCli().cmdloop()
